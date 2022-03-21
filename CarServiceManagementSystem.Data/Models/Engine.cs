5096b0a51e1a0d709daa3a57a92b64bb1ce42802
MariyanTsonev <mariyan.p.tsonev@gmail.com>
2022-03-21 23:29:03 +0200

    Added validation attributes

--- CarServiceManagementSystem.Data/Models/Engine.cs
+++ CarServiceManagementSystem.Data/Models/Engine.cs
@@ -1,6 +1,7 @@
 ﻿using CarServiceManagementSystem.Data.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,16 +17,22 @@ namespace CarServiceManagementSystem.Data.Models
 
         public int Id { get; set; }
 
+        [Range(40, 9300, ErrorMessage = "Engine Displacement cannot be less than 40 and more than 9300 cubic centimeters!")]
         public int Displacement { get; set; }
 
-        public double Power { get; set; }
+        [Range(0, 850, ErrorMessage = "Engine Horsepower must be a positive number, no higher than 850!")]
+        public double HorsePower { get; set; }
 
+        [MaxLength(25, ErrorMessage = "Engine code must be no longer than 25 characters!")]
         public string EngineCode { get; set; }
 
+        [Range(1, 16, ErrorMessage = "Invalid cylinder count!")] // Can create custom attribute to exlucde 7,9,11,13,14,15 cylinder count
         public int CylindersCount { get; set; }
 
+        [Range(0, 1100, ErrorMessage = "Engine Torque must be a positive number, no higher than 1100!")]
         public double Torque { get; set; }
 
+        [Required]
         public FuelType FuelType { get; set; }
 
         public ICollection<Car> Cars { get; set; }
