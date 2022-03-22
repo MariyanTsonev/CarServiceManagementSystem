06c46e6f896fe8a75e9c16cdfa6746f0609bdd57
MariyanTsonev <mariyan.p.tsonev@gmail.com>
2022-03-22 21:43:22 +0200

    Added validation attributes

--- CarServiceManagementSystem.Data/Models/Repair.cs
+++ CarServiceManagementSystem.Data/Models/Repair.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,18 +16,24 @@ namespace CarServiceManagementSystem.Data.Models
 
         public int Id { get; set; }
 
+        //TODO date validation
         public DateTime Date { get; set; }
 
+        [Required]
         public int CarId { get; set; }
 
         public Car Car { get; set; }
 
+        [Required]
         public string UserId { get; set; }
 
         public ApplicationUser User { get; set; }
 
+        [Required]
+        [StringLength(650, ErrorMessage = "Repair description is too long!")]
         public string Description { get; set; }
 
+        [Range(1, 999999, ErrorMessage = "Odometer reading must be between 1 and 999999!")]
         public int Odometer { get; set; }
 
         public Oil ChangedOil { get; set; }
