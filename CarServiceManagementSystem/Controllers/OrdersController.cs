fd6901d72bb91c2a8d9b3020de8eaef4f1ebba37
MariyanTsonev <mariyan.p.tsonev@gmail.com>
2022-03-23 00:18:22 +0200

    Added new controllers

---
+++ CarServiceManagementSystem/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using CarServiceManagementSystem.ViewModels.Orders;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarServiceManagementSystem.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public OrdersController()
+        {
+
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(NewOrderInputModel model)
+        {
+            return null;
+        }
+    }
+}
---
+++ CarServiceManagementSystem/Controllers/UsersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarServiceManagementSystem.Controllers
+{
+    public class UsersController : Controller
+    {
+
+    }
+}
