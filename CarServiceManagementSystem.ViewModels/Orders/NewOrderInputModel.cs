2eeef0d18be2805be3377d3bb5a3b19b959501e7
MariyanTsonev <mariyan.p.tsonev@gmail.com>
2022-03-23 00:22:26 +0200

    Added Services and ViewModels projects with error view model and new order input model

---
+++ CarServiceManagementSystem.ViewModels/Orders/NewOrderInputModel.cs
@@ -0,0 +1,40 @@
+﻿using CarServiceManagementSystem.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceManagementSystem.ViewModels.Orders
+{
+    public class NewOrderInputModel
+    {
+        public DateTime Date { get; set; }
+
+        public string IssueDescription { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string CarMake { get; set; }
+
+        public string CarModel { get; set; }
+
+        public int Year { get; set; }
+
+        public int Month { get; set; }
+
+        public string LicencePlate { get; set; }
+
+        public string VinNumber { get; set; }
+        
+        public FuelType FuelType { get; set; }
+
+        public int Displacement { get; set; }
+
+        public double HorsePower { get; set; }
+
+        public string EngineCode { get; set; }
+
+        public int CylindersCount { get; set; }
+    }
+}
