57f0fdee81c597202e96abcd67110f32164f106e
MariyanTsonev <mariyan.p.tsonev@gmail.com>
2022-03-31 23:57:53 +0300

    Added migration to include new OnDelete behavior for car entity

--- CarServiceManagementSystem.Data/Migrations/20220331204915_AddCarEntityDeleteBehavior.Designer.cs
+++ CarServiceManagementSystem.Data/Migrations/20220331204915_AddCarEntityDeleteBehavior.Designer.cs
@@ -4,6 +4,7 @@ using CarServiceManagementSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarServiceManagementSystem.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220331204915_AddCarEntityDeleteBehavior")]
+    partial class AddCarEntityDeleteBehavior
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CarServiceManagementSystem.Data/Migrations/20220331204915_AddCarEntityDeleteBehavior.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarServiceManagementSystem.Data.Migrations
+{
+    public partial class AddCarEntityDeleteBehavior : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Cars_CarId",
+                table: "Orders");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Cars_CarId",
+                table: "Orders",
+                column: "CarId",
+                principalTable: "Cars",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Cars_CarId",
+                table: "Orders");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Cars_CarId",
+                table: "Orders",
+                column: "CarId",
+                principalTable: "Cars",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
