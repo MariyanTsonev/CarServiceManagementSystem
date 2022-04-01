35a703592bb9d4d16ccf53de1009c1c0ceaa7932
MariyanTsonev <mariyan.p.tsonev@gmail.com>
2022-04-01 15:50:21 +0300

    Added new property to car entity

---
+++ CarServiceManagementSystem.Data/Migrations/20220401124644_AddOdometerPropertyToCarEntity.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarServiceManagementSystem.Data.Migrations
+{
+    public partial class AddOdometerPropertyToCarEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Odometer",
+                table: "Cars",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Odometer",
+                table: "Cars");
+        }
+    }
+}
